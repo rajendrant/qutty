a59d7754bf685f575797275cfe6dad9115f3243b
Rajendran Thirupugalsamy <rajendran.thirupugal@gmail.com>
2012-03-25 22:31:17 -0700

    Initial commit of putty src

---
+++ puttysrc/QtStuff.h
@@ -0,0 +1,78 @@
+#ifndef QTSTUFF_H
+#define QTSTUFF_H
+
+#include <stdio.h>
+#include <windows.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct Filename {
+    char path[FILENAME_MAX];
+};
+
+struct FontSpec {
+    char name[64];
+    int isbold;
+    int height;
+    int charset;
+};
+
+typedef uint32_t uint32;
+
+typedef void* Context;
+
+void init_ucs(Config *, struct unicode_data *);
+
+/*
+ * On Windows, copying to the clipboard terminates lines with CRLF.
+ */
+#define SEL_NL { 13, 10 }
+
+#define DEFAULT_CODEPAGE CP_ACP
+
+#define TICKSPERSEC 1000	       /* GetTickCount returns milliseconds */
+
+#define GETTICKCOUNT GetTickCount
+
+#define f_open(filename, mode, isprivate) ( fopen((filename).path, (mode)) )
+
+/*
+ * sk_getxdmdata() does not exist under Windows (not that I
+ * couldn't write it if I wanted to, but I haven't bothered), so
+ * it's a macro which always returns NULL. With any luck this will
+ * cause the compiler to notice it can optimise away the
+ * implementation of XDM-AUTHORIZATION-1 in x11fwd.c :-)
+ */
+#define sk_getxdmdata(socket, lenp) (NULL)
+
+
+#define APPNAME "PuTTY"
+
+void qt_message_box(void * frontend, char *title, char *msg);
+
+#define qt_critical_msgbox(frontend, fmt, ...) do { \
+    char qt_message_box_msg[1000]; \
+    _snprintf(qt_message_box_msg, sizeof(qt_message_box_msg), fmt, __VA_ARGS__); \
+    qt_message_box(frontend, APPNAME " Fatal Error", qt_message_box_msg); \
+} while(0)
+
+#define modalfatalbox(fmt, ...) qt_message_box(NULL, APPNAME " Fatal Error", fmt)
+
+#define connection_fatal(frontend, fmt, ...) qt_critical_msgbox(frontend, fmt, __VA_ARGS__)
+
+#define fatalbox(fmt, ...) qt_critical_msgbox(NULL, fmt, __VA_ARGS__)
+
+#define assert(cond) do {\
+    if(!(cond)) fatalbox("fatal assert %s(%d)"#cond, __FUNCTION__, __LINE__); \
+} while(0)
+
+//void notify_remote_exit(void *fe)
+#define notify_remote_exit(frontend)
+
+#ifdef __cplusplus
+}
+#endif
+#endif // QTSTUFF_H
---
+++ puttysrc/missing.h
@@ -0,0 +1,18 @@
+#ifndef __MISSING_H__
+#define __MISSING_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "PUTTY.H"
+
+//void noise_ultralight(unsigned long data) TODO
+#define noise_ultralight(d)
+
+//void update_specials_menu(void *frontend)
+#define update_specials_menu(a)
+
+#ifdef __cplusplus
+}
+#endif
+#endif // __MISSING_H__
