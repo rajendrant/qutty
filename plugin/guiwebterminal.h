f1a8fecbbaca6c9b43a5349093b22d2d8b74ea8c
Rajendran Thirupugalsamy <rajendran.thirupugal@gmail.com>
2015-03-09 12:34:18 -0700

    variable length array not supported/enabled in VC++

--- plugin/guiwebterminal.h
+++ plugin/guiwebterminal.h
@@ -16,9 +16,10 @@ public:
     ~GuiWebTerminal(){}
 
     Q_INVOKABLE void sendKeys(QString str) {
-        wchar_t buf[str.length()+1];
+        wchar_t *buf = new wchar_t[str.length()+1];
         str.toWCharArray(buf);
         luni_send(termWnd->term->ldisc, buf, str.length(), 0);
+        delete buf;
     }
 };
 
